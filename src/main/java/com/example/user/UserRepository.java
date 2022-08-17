c8107d12976c3d79fa0581017243e3cf21850df9
Bachhsunkid <86517817+Bachhsunkid@users.noreply.github.com>
2022-08-17 10:08:07 +0700

    delete

--- src/main/java/com/example/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-	public Long countById(Integer id); //custom nhung theo tieu chuan cua data jpa
-}
